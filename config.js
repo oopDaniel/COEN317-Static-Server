049bbcbfb94a432ed74bdf7c208c2d1275c92408
Daniel <oopdaniel@gmail.com>
2019-01-26 18:42:33 -0800

    solve issues for passing arguments from command line

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 let PORT = 8080
 let ROOT = './static'
-
 const args = process.argv.slice(2)
-args.forEach(arg => {
-  const [key, value] = arg.split('=')
-  if (key === 'port') PORT = value
-  if (key === 'document_root') ROOT = value
-})
+
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === '-port') PORT = args[i++ + 1]
+  if (args[i] === '-document_root') ROOT = args[i++ + 1]
+}
 
 export { PORT, ROOT }
