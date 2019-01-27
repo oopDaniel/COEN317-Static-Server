e0edda1f3ec89485b2bea43d6b6a24260b00c5c6
Daniel <oopdaniel@gmail.com>
2019-01-26 21:54:39 -0800

    add 403 page

--- server/server.js
+++ server/server.js
@@ -2,20 +2,21 @@ import path from 'path'
 import fs from 'fs'
 import getContentType from './contentTypeResolver'
 
-export default ROOT => (req, res, next) => {
+export default ROOT => (req, res) => {
+  // Helper function to send file
   const sendFile = sendFileToRes(res)
 
-  // Resolve the path and file name, and query index.html if needed
+  // Resolve the path and file name, and set query to index.html if needed
+  // This will disable accessing directory as well
   const rootPath = path.resolve(ROOT)
   let fileName = path.join(rootPath, req.url)
-  const page404 = path.join(rootPath, '404.html')
   if (fileName.endsWith('/')) fileName += 'index.html'
 
   fs.stat(fileName, (err, stat) => {
-
     // There's no such file
     if (err) {
       res.statusCode = 404
+      const page404 = path.join(rootPath, '404.html')
       const { size } = fs.statSync(page404)
       res.setHeader('Content-Length', size)
       res.setHeader('Content-Type', 'text/html')
@@ -27,12 +28,16 @@ export default ROOT => (req, res, next) => {
       fs.accessSync(fileName, fs.constants.R_OK)
     } catch (err) {
       res.statusCode = 403
-      return res.end()
+      const page403 = path.join(rootPath, '403.html')
+      const { size } = fs.statSync(page403)
+      res.setHeader('Content-Length', size)
+      res.setHeader('Content-Type', 'text/html')
+      return sendFile(page403)
     }
 
-    // Support 304 Not modified
+    // Support 304 Not Modified to enable browser cache
     const mtime = new Date(stat.mtimeMs).toUTCString()
-    if (req.headers['if-modified-since'] === mtime){
+    if (req.headers['if-modified-since'] === mtime) {
       res.statusCode = 304
       return res.end()
     }
