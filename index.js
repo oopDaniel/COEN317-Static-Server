8ed9961513310aee3ced675ba75a16a179ab5b48
Daniel <oopdaniel@gmail.com>
2019-02-01 18:41:06 -0800

    rename files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import http from 'http'
-import server from './server/server.js'
-import { PORT, ROOT } from './server/config.js'
+import server from './serve/server.js'
+import { PORT, ROOT } from './serve/config.js'
 
 http.createServer(server(ROOT))
   .listen(PORT, () => console.log(`Listening to ${PORT} for files on ${ROOT}.`))
